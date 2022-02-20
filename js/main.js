ac1cf47bcad06dc1ed1405add49a86a38ca09729
Vladimir-user <iv_vs@mail.ru>
2022-02-21 00:45:24 +0300

    Поизучал выбрасывание исключительных ситуаций

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 function getRandomNum (min, max) {
-  if (min>=0 && max>=0) {
-    return Math.floor(Math.random()*(max-min) + min);
+  try {
+    if (min <= 0 || max <=0 ) {
+      throw {
+        name: 'NegativeNumber',
+        message: 'Используйте только положительные числа'
+      };
+    } else if (max < min) {
+      throw {
+        name: 'UnequalNumber',
+        message: `Некорректный диапазон ${  min  } болше ${  max  }!`
+      };
+    } else {
+      return Math.floor(Math.random()*(max-min) + min);
+    }
+  } catch (error) {
+    if (error.name === 'NegativeNumber') {
+      throw error.message;
+    } else if (error.name === 'UnequalNumber') {
+      throw error.message;
+    }
   }
-  return ('Используйте только положительные числа');
 }
-
 getRandomNum (2, 5);
 
 function checkCommentLength (comment, maxLength) {
   if (comment.length <= maxLength) {
-    return true;
+    return comment.length <= maxLength;
   }
-  return false;
+  return comment.length <= maxLength;
 }
 
 checkCommentLength('hello', 5);
