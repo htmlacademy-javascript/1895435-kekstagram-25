114048c6150a6886a2a2b62f0459e51d8810c3d3
Vladimir-user <iv_vs@mail.ru>
2022-02-21 11:07:42 +0300

    Использовал стрелочные функции

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-function getRandomNum (min, max) {
+const getRandomNum = (min, max) => {
+  if (min >= 0 && max >= 0 && min < max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  throw new Error(`Используйте только положительные числа. Или превое число ${min} болше конечного ${max} !`);
+};
+
+getRandomNum(2, 5);
+
+const getRandomNumCatch = (min, max) => {
   try {
-    if (min <= 0 || max <=0 ) {
+    if (min <= 0 || max <= 0) {
       throw {
         name: 'NegativeNumber',
         message: 'Используйте только положительные числа'
@@ -8,10 +17,10 @@ function getRandomNum (min, max) {
     } else if (max < min) {
       throw {
         name: 'UnequalNumber',
-        message: `Некорректный диапазон ${  min  } болше ${  max  }!`
+        message: `Некорректный диапазон ${min} болше ${max}!`
       };
     } else {
-      return Math.floor(Math.random()*(max-min) + min);
+      return Math.floor(Math.random() * (max - min) + min);
     }
   } catch (error) {
     if (error.name === 'NegativeNumber') {
@@ -20,14 +29,13 @@ function getRandomNum (min, max) {
       throw error.message;
     }
   }
-}
-getRandomNum (2, 5);
+};
 
-function checkCommentLength (comment, maxLength) {
-  if (comment.length <= maxLength) {
-    return comment.length <= maxLength;
-  }
-  return comment.length <= maxLength;
-}
+getRandomNumCatch(2, 9);
+
+const checkCommentLength = (comment, maxLength) => {
+  const check = comment.length <= maxLength;
+  return check;
+};
 
 checkCommentLength('hello', 5);
