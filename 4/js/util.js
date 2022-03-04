7f5b3e3a235b593aa3dadb3d0a2574d9256237a2
Keks <keks@htmlacademy.ru>
2022-03-04 18:13:59 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/util.js
@@ -0,0 +1,15 @@
+const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const getArrayElement = (arr) => arr[getRandomNum(0, arr.length - 1)];
+
+const getTextMessage = (arr) => {
+  let value = getRandomNum(1, 2);
+  let text = '';
+  while (value !== 0) {
+    text += getArrayElement(arr);
+    value--;
+  }
+  return text;
+};
+
+export { getRandomNum, getArrayElement, getTextMessage };
