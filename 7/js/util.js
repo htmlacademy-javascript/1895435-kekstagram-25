7de03deb8ae4169710394d8d646d549e66e1c797
Keks <keks@htmlacademy.ru>
2022-03-28 17:01:17 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/util.js
@@ -0,0 +1,17 @@
+const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const getArrayElement = (arr) => arr[getRandomNum(0, arr.length - 1)];
+
+const getTextMessage = (arr) => {
+  let value = getRandomNum(1, 2);
+  let text = '';
+  while (value !== 0) {
+    text += getArrayElement(arr);
+    value--;
+  }
+  return text;
+};
+
+const compareLength = (value, max) => value.length <= max;
+
+export { getRandomNum, getArrayElement, getTextMessage, compareLength};
