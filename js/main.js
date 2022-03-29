f79e4d29abb3d02caf0d3f6cde8b6eb052070ce8
Vladimir-user <iv_vs@mail.ru>
2022-03-29 17:39:33 +0300

    В работе

--- js/main.js
+++ js/main.js
@@ -2,46 +2,11 @@ import {getPhotos} from './loadingdata.js';
 import {getMiniatures} from './miniatures.js';
 import {openModalWindow} from './viewpicture.js';
 import './uploadfile.js';
-//import {Scale} from './changescale.js';
+import './changescale.js';
+import './effectslider.js';
 
 const photosData = getPhotos(25);
 
 getMiniatures(photosData);
 
 openModalWindow(photosData);
-
-//const zn = Scale();
-
-
-function Scale (val) {
-  let count = val;
-  return {
-    more:function() {
-      count +=25;
-    },
-    less:function() {
-      count -=25;
-    },
-    value:function() {
-      return count;
-    }
-  };
-}
-
-var makeCounter = function() {
-  var privateCounter = 0;
-  function changeBy(val) {
-    privateCounter += val;
-  }
-  return {
-    increment: function() {
-      changeBy(1);
-    },
-    decrement: function() {
-      changeBy(-1);
-    },
-    value: function() {
-      return privateCounter;
-    }
-  }
-};
