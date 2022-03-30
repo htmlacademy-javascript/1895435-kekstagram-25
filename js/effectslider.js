b1b4e8127352e94d00b2c97f65908243b5b25268
Vladimir-user <iv_vs@mail.ru>
2022-03-30 17:32:32 +0300

    В работе

--- js/effectslider.js
+++ js/effectslider.js
@@ -1,9 +1,20 @@
 const sliderElement = document.querySelector('.effect-level__slider');
 
+const EFFECT_CHROME = {range: {min: 0, max: 1}, start: 1, step: 0.1};
+const EFFECT_SEPIA = {range: {min: 0, max: 1}, start: 1, step: 0.1};
+const EFFECT_MARVIN = {range: {min: 0, max: 100}, start: 100, step: 1, format: {to: (value) => `${value}%`}};
+const EFFECT_PHOBOS = {range: {min: 0, max: 3}, start: 3, step: 0.1, format: {to: (value) => `${value.toFixed(1)}px`}};
+const EFFECT_HEAT = {range: {min: 0, max: 3}, start: 1, step: 0.1};
+
 noUiSlider.create(sliderElement, {
-  range: {
-    min: 0,
-    max: 100,
-  },
-  start: 80,
+  range: {min: 1, max: 3},
+  start: 3,
+  step: 0.1,
+  connect: 'lower',
+  format: {to: (value) => value.toFixed(1), from: (value) => parseFloat(value)}
 });
+
+const switchEffects = {
+  'effect-chrome': {'effect': EFFECT_CHROME, 'filter': 'grayscale'},
+
+};
