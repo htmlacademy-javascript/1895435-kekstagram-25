0a6211d52ddfe69279b605e13bc038f063d14e91
Keks <keks@htmlacademy.ru>
2022-04-04 18:04:58 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/effectslider.js
+++ 8/js/effectslider.js
@@ -2,6 +2,7 @@ const sliderElement = document.querySelector('.effect-level__slider');
 const effectsList = document.querySelector('.effects__list');
 const effectLevel = document.querySelector('.effect-level__value');
 const imagePreview = document.querySelector('.img-upload__preview');
+const slaiderField = document.querySelector('.img-upload__effect-level');
 
 const EFFECT_CHROME = {range: {min: 0, max: 1}, start: 1, step: 0.1, format: {to: (value) => value.toFixed(1), from: (value) => parseFloat(value)}};
 const EFFECT_SEPIA = {range: {min: 0, max: 1}, start: 1, step: 0.1, format: {to: (value) => value.toFixed(1), from: (value) => parseFloat(value)}};
@@ -10,16 +11,16 @@ const EFFECT_PHOBOS = {range: {min: 0, max: 3}, start: 3, step: 0.1, format: {to
 const EFFECT_HEAT = {range: {min: 0, max: 3}, start: 3, step: 0.1, format: {to: (value) => value.toFixed(1), from: (value) => parseFloat(value)}};
 
 const switchEffects = {
-  chrome: {'effect': EFFECT_CHROME, 'filter': 'grayscale'},
-  sepia: {'effect': EFFECT_SEPIA, 'filter': 'sepia'},
-  marvin: {'effect': EFFECT_MARVIN, 'filter': 'invert'},
-  phobos: {'effect': EFFECT_PHOBOS, 'filter': 'blur'},
-  heat: {'effect': EFFECT_HEAT, 'filter': 'brightness'}
+  chrome: [EFFECT_CHROME, 'grayscale'],
+  sepia: [EFFECT_SEPIA, 'sepia'],
+  marvin: [EFFECT_MARVIN, 'invert'],
+  phobos: [EFFECT_PHOBOS, 'blur'],
+  heat: [EFFECT_HEAT, 'brightness']
 };
 
 const getEffectSlider = () => {
   noUiSlider.create(sliderElement, {range: {min: 1, max: 3}, start: 3, step: 0.1, connect: 'lower'});
-  sliderElement.hidden = true;
+  slaiderField.hidden = true;
   imagePreview.style.filter = 'none';
 };
 
@@ -27,15 +28,15 @@ const onEffectElementClick = (evt) => {
   if(evt.target.tagName === 'INPUT') {
     const effectKey = evt.target.closest('input').value;
     if (effectKey !== 'none') {
-      sliderElement.hidden = false;
-      sliderElement.noUiSlider.updateOptions(switchEffects[effectKey]['effect']);
+      slaiderField.hidden = false;
+      sliderElement.noUiSlider.updateOptions(switchEffects[effectKey][0]);
       sliderElement.noUiSlider.on('update', () => {
         effectLevel.value = sliderElement.noUiSlider.get();
-        imagePreview.style.filter = `${switchEffects[effectKey]['filter']}(${sliderElement.noUiSlider.get()})`;
+        imagePreview.style.filter = `${switchEffects[effectKey][1]}(${sliderElement.noUiSlider.get()})`;
       });
     } else {
       imagePreview.style.filter = 'none';
-      sliderElement.hidden = true;
+      slaiderField.hidden = true;
     }
   }
 };
