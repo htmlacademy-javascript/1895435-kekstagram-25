248aca404072adcb5b259127d6d67ed97ae212f9
Vladimir-user <iv_vs@mail.ru>
2022-04-05 17:56:32 +0300

    В работе

--- js/dataserver.js
+++ js/dataserver.js
@@ -1,7 +1,29 @@
-const getPhotos = (fn) => {
-  fetch('https://25.javascript.pages.academy/kekstagram/data')
-    .then((response) => response.json())
-    .then((photos) => fn(photos));
+const showAlert = (message) => {
+  alert(message);
+};
+/*
+const getPhotos = async (fn1, fn2) => {
+  try {
+    fetch('https://25.javascript.pages.academy/kekstagram/data1')
+      //.then((res) => res.ok ? res : Promise.reject(res))
+      .then((response) => response.json())
+      .then((photos) => {
+        fn1(photos);
+        fn2(photos);
+      });
+  } catch(err) {
+    showAlert('Hello');
+    console.error('Hello', err);
+  }
+};
+*/
+const getPhotos = async(url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw showAlert('Data');
+  }
+  const data = await res.json();
+  return data;
 };
 
 export {getPhotos};
--- js/main.js
+++ js/main.js
@@ -5,10 +5,23 @@ import './uploadfile.js';
 import './changescale.js';
 import './effectslider.js';
 
-getPhotos(getMiniatures);
+//getPhotos(getMiniatures, openModalWindow);
 
-getPhotos(openModalWindow);
+//getPhotos(openModalWindow);
 
 //getMiniatures(photosData);
 
 //openModalWindow(photosData);
+
+const showAlert = (message) => {
+  alert(message);
+};
+
+getPhotos('https://25.javascript.pages.academy/kekstagram/data1')
+  .then((data) => {
+    getMiniatures(data);
+    openModalWindow(data);
+  })
+  .catch((err) => {
+    showAlert('Not found');
+  });
